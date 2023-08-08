76b6338d59e2b2e567a9150095f606bb91c04277
Volodymyr <volodymyrdavidov1@gmail>
2023-08-08 13:06:43 +0300

    Add Dto validation and CustomGlobalExceptionHandler.java

--- src/main/java/com/example/demo/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/demo/dto/CreateBookRequestDto.java
@@ -1,15 +1,29 @@
 package com.example.demo.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
     private Long id;
+    @NotEmpty
     private String title;
+    @NotEmpty
     private String author;
+    @NotNull
+    @Pattern(regexp = "^(978|979)-\\d{1,5}-\\d{2,7}-\\d{1,6}-[\\dX]$")
     private String isbn;
+    @NotNull
+    @Min(0)
+    @Positive
     private BigDecimal price;
+    @NotEmpty
     private String description;
+    @NotEmpty
     private String coverImage;
 }
